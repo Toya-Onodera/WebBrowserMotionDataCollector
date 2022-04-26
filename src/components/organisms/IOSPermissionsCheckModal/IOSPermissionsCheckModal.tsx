b3e13b3a522247476eee0086738a2947c0572aee
Toya-Onodera <gtrzexel@gmail.com>
2022-04-26 12:11:39 +0900

    Update: Tingle.js ではなく MUI の Dialog 使った権限確認に変更

--- src/components/organisms/IOSPermissionsCheckModal/IOSPermissionsCheckModal.tsx
+++ src/components/organisms/IOSPermissionsCheckModal/IOSPermissionsCheckModal.tsx
@@ -1,71 +1,61 @@
-import React, { useEffect } from "react";
-import tingle from "tingle.js";
+import React, { useCallback, useEffect, useState } from "react";
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 export const IOSPermissionsCheckModal: React.VFC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
-    // FIXME: iOS対応の為必要とする
-    // https://qiita.com/Toya-Onodera/items/fe06000a7909797fb5b4
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if (
+      // FIXME: iOS対応の為必要とする
+      // https://qiita.com/Toya-Onodera/items/fe06000a7909797fb5b4
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       typeof (DeviceMotionEvent as any).requestPermission === "function" &&
       sessionStorage.getItem("isPermission") === null
     ) {
-      const tingleLinkElement = document.createElement("link");
-      tingleLinkElement.rel = "stylesheet";
-      tingleLinkElement.href =
-        "https://cdnjs.cloudflare.com/ajax/libs/tingle/0.15.3/tingle.min.css";
-      document.head.appendChild(tingleLinkElement);
-
-      tingleLinkElement.onload = () => {
-        const tingleScriptElement = document.createElement("script");
-        tingleScriptElement.src =
-          "https://cdnjs.cloudflare.com/ajax/libs/tingle/0.15.3/tingle.min.js";
-        document.body.appendChild(tingleScriptElement);
-
-        tingleScriptElement.onload = () => {
-          const modal = new tingle.modal({
-            footer: true,
-          });
-
-          modal.setContent("<p>このサイトでは、センサー値を扱います。</p>");
-          modal.addFooterBtn(
-            "Cancel",
-            "tingle-btn tingle-btn--default tingle-btn--pull-right",
-            () => modal.close(),
-          );
-          modal.addFooterBtn(
-            "OK",
-            "tingle-btn tingle-btn--primary tingle-btn--pull-right",
-            () => {
-              modal.close();
-            },
-          );
-
-          document
-            .querySelector(
-              ".tingle-btn.tingle-btn--primary.tingle-btn--pull-right",
-            )
-            ?.addEventListener("click", async () => {
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              const isDeviceOrientationEvent = await (
-                DeviceOrientationEvent as any
-              ).requestPermission();
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              const isDeviceMotionEvent = await (
-                DeviceMotionEvent as any
-              ).requestPermission();
-
-              // 許可したあとはまた許可が必要になるまでポップアップが出ないようにする
-              if (isDeviceOrientationEvent && isDeviceMotionEvent) {
-                sessionStorage.setItem("isPermission", "true");
-              }
-            });
-
-          modal.open();
-        };
-      };
+      setIsOpen(true);
     }
   }, []);
 
-  return <></>;
+  const handleClose = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
+
+  const handleCheckPermissions = useCallback(async () => {
+    handleClose();
+
+    const isDeviceOrientationEvent = await (DeviceOrientationEvent as any) // eslint-disable-line @typescript-eslint/no-explicit-any
+      .requestPermission();
+    const isDeviceMotionEvent = await (DeviceMotionEvent as any) // eslint-disable-line @typescript-eslint/no-explicit-any
+      .requestPermission();
+
+    // 許可したあとはまた許可が必要になるまでポップアップが出ないようにする
+    if (isDeviceOrientationEvent && isDeviceMotionEvent) {
+      sessionStorage.setItem("isPermission", "true");
+    }
+  }, [handleClose]);
+
+  return (
+    <Dialog open={isOpen}>
+      <DialogTitle id="alert-dialog-title">センサー利用確認</DialogTitle>
+
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          このサイトでは、センサー値を扱います。
+        </DialogContentText>
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={handleClose}>キャンセル</Button>
+        <Button onClick={handleCheckPermissions}>許可</Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
